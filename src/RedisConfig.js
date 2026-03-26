5cb315e8ec8e06f0aa88aa687bbf3863510fe23d
Julius Lopes <juliuscxlopes@gmail.com>
2026-03-26 13:49:15 -0300

    🚀 retirada stream data

--- src/RedisConfig.js
+++ src/RedisConfig.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 class RedisConfig {
   constructor() {
     this.client = new Redis({
-      host: process.env.REDIS_HOST || '127.0.0.1',
-      port: process.env.REDIS_PORT || 6379,
-      password: process.env.REDIS_PASSWORD || undefined,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      //password: process.env.REDIS_PASSWORD,
       retryStrategy: (times) => Math.min(times * 50, 2000),
     });
 
